2778419c486ed63016a04f8490ee87e52fbc93f8
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-08-12 18:04:59 +0430

    useParams() --> show id of that i click it

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,6 +15,7 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
+  console.log(useParams())//show id of singleProduct that i click it
   return <h4>single product page</h4>
 }
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,5 @@
 export const formatPrice = (number) => {
 	const newNumber = Intl.NumberFormat('fa-IR').format(number) + ' ت'
-
-	console.log(newNumber)
 	return newNumber
 }
 
