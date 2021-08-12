298130848d85f06ee0e71349b088965f0cb11fb8
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-08-12 12:04:30 +0430

    show price to rial of iranian currency BUT: i want show as Tooman

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+	return new Intl.NumberFormat('fa-IR', {currency: 'IRR', style: 'currency'}).format(number * 10)
+}
 
 export const getUniqueValues = () => {}
