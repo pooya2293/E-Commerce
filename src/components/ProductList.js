cffdbe4cc96a53265e1f8f53f4e2f147934746b8
pooya2293 <poorya2293@gmail.com>
2021-08-28 13:43:07 +0430

    make list view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,12 +4,14 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const {filtered_products:products} = useFilterContext();
-  return (
-    <>
-      <GridView products={products}>products list</GridView>  
-    </>
-  )
+  const {filtered_products:products,grid_view} = useFilterContext();
+  if(products.length == 0){
+    return <h4 className={{ textTransform : 'none' }}>Sorry,no products matched you search...</h4>
+  }
+  if(grid_view === false){
+    return <ListView products={products} />
+  }
+  return <GridView products={products}>products list</GridView>
 }
 
 export default ProductList
