e0a414fd7c45adf90f2b5d47d520fae1c8630ac5
pooya2293 <poorya2293@gmail.com>
2021-08-29 12:06:25 +0430

    give option witch i click from to form and path to sort payload

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -25,6 +25,11 @@ const filter_reducer = (state, action) => {
   if(action.type === SET_GRIDVIEW){
     return {...state,grid_view:true }
   }
+
+  if(action.type === UPDATE_SORT){
+    return { ...state,sort:action.payload }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
