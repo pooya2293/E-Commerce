af0755c15b08d990415964196d453cd5ebc974cd
pooya2293 <poorya2293@gmail.com>
2021-08-30 12:58:37 +0430

    show evry seperate company in DOM when click each of them path to filters.company in filterContext

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -65,6 +65,20 @@ const Filters = () => {
             </div>
           </div>
         {/*end category*/}
+        {/*company*/}
+          <div className="form-control">
+            <h5>company</h5>
+            <select 
+              name="company" 
+              value={company} 
+              onChange={updateFilters} 
+              className="company">
+                {companies.map((c,index)=>{
+                  return <option key={index} value={c}>{c}</option>
+                })}
+            </select>
+          </div>
+        {/*end company*/}
         </form>
       </div>
     </Wrapper>
